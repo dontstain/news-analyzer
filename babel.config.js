8211eb44b09a1a7f7a7c11a3129db67902c95855
Dmitry Tripolsky <donotstain@yandex.ru>
2019-12-08 14:30:30 +0400

    fixed plugins' versions, fixed html head and included all browsers except Y

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,11 @@ const presets = [
     "@babel/env",
     {
       targets: { 
-        edge: "17",
-        ie: "11",
-        firefox: "50",
         chrome: "64",
+        android: "64",
+        firefox: "50",
         safari: "11.1",
+        edge: "15"
       },
       useBuiltIns: "usage",
       corejs: "3.4.1" 

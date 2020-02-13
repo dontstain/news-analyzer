c691a0b983a095c79a4d4579fb723c8f144c048b
dontstain <dnostain@gmail.com>
2020-02-13 16:45:11 +0400

    fixed changing blocks

--- src/js/utils/render-block.js
+++ src/js/utils/render-block.js
@@ -6,18 +6,21 @@ function renderBlock(block) {
       RESULT.classList.remove('search-result_hidden');
       if (!NO_RESULT.classList.contains('no-results__fail_hidden')) NO_RESULT.classList.add('no-results__fail_hidden');
       if (!LOADING.classList.contains('no-results__loading_hidden')) LOADING.classList.add('no-results__loading_hidden');
+      NO_RESULT.parentElement.classList.add('no-results_hidden');
       break;
       
     case NO_RESULT:
       if (!RESULT.classList.contains('search-result_hidden')) RESULT.classList.add('search-result_hidden');
       NO_RESULT.classList.remove('no-results__fail_hidden');
       if (!LOADING.classList.contains('no-results__loading_hidden')) LOADING.classList.add('no-results__loading_hidden');
+      NO_RESULT.parentElement.classList.remove('no-results_hidden');
       break;
 
     case LOADING:
       if (!RESULT.classList.contains('search-result_hidden')) RESULT.classList.add('search-result_hidden');
       if (!NO_RESULT.classList.contains('no-results__fail_hidden')) NO_RESULT.classList.add('no-results__fail_hidden');
       LOADING.classList.remove('no-results__loading_hidden');
+      LOADING.parentElement.classList.remove('no-results_hidden');
       break;
   }
 }
